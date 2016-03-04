62d08d33f58163e071662ef2a67a3a114a1bf598
Isdemus <isdemus@gmail.com>
2016-03-03 22:42:55 -0500

    push test

---
+++ abc#1.cpp
@@ -0,0 +1,17 @@
+//ABC #1
+//linked list implementation
+
+//INITIAL Plan: basic structure definition
+
+#include <iostream>
+using namespace std;
+
+struct list {
+};
+
+
+int main(int argc, char* argv[]) {
+	cout << "program begein" << endl;
+	cout << "program terminated" << endl;
+	return 0;
+}
