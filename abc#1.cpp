538be0eaa1b0a5e55d23254e601bc82a7b208eac
Isdemus <isdemus@gmail.com>
2016-03-03 22:53:08 -0500

    initial

--- abc#1.cpp
+++ abc#1.cpp
@@ -7,11 +7,22 @@
 using namespace std;
 
 struct list {
+	list *next;
+	list *prev;
+	int data1; 
 };
-
 
 int main(int argc, char* argv[]) {
 	cout << "program begein" << endl;
+
+	list firstObject;
+	firstObject.next = NULL;
+	firstObject.prev = NULL;
+	firstObject.data1 = 100;
+
+	cout << firstObject.data1 << endl;
+
 	cout << "program terminated" << endl;
+
 	return 0;
 }
