75a1cc605a6188c99cbfaad57a3f12484ac80c7f
Isdemus <isdemus@gmail.com>
2016-03-03 23:08:01 -0500

    struct_explore

--- abc#1.cpp
+++ abc#1.cpp
@@ -7,20 +7,34 @@
 using namespace std;
 
 struct list {
-	list *next;
-	list *prev;
+	list* next;
+	list* prev;
 	int data1; 
 };
+
+//automated function to build a list
+void list_generator(struct list* object, struct list* next, struct list* prev, int data1) {
+	object->next = next;
+	object->prev = prev;
+	object->data1 = data1;
+}
 
 int main(int argc, char* argv[]) {
 	cout << "program begein" << endl;
 
-	list firstObject;
-	firstObject.next = NULL;
-	firstObject.prev = NULL;
-	firstObject.data1 = 100;
+	struct list firstObject;
+	list_generator(&firstObject, NULL, NULL, 100);
 
+	struct list* secondObject = new list;
+	list_generator(secondObject, NULL, NULL, 50);
+
+	struct list* thirdObject = (list*) malloc(sizeof(struct list));
+	list_generator(thirdObject, NULL, NULL, 25);
+
+	/* checking area */
 	cout << firstObject.data1 << endl;
+	cout << secondObject->data1 << endl;
+	cout << thirdObject->data1 << endl;
 
 	cout << "program terminated" << endl;
 
