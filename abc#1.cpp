4a59609e5479e39795c2410b204697c4ceeeb1c5
Isdemus <isdemus@gmail.com>
2016-03-03 23:40:53 -0500

    enum and union designed

--- abc#1.cpp
+++ abc#1.cpp
@@ -4,40 +4,52 @@
 //INITIAL Plan: basic structure definition
 
 #include <iostream>
+#include <string>
 using namespace std;
+
+enum month_t {jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec};
+
+union data {
+	int int_data;
+	//string string_data;
+	char char_data;
+	bool bool_data;
+};
 
 typedef struct list{
 	struct list* next;
 	struct list* prev;
-	int data1;
+	union data list_data;
+	enum month_t month;
 } List;
 
 //automated function to build a list
-void list_generator(List* object, List* next, List* prev, int data1) {
+void list_generator(List* object, List* next, List* prev, int data1, enum month_t month) {
 	object->next = next;
 	object->prev = prev;
-	object->data1 = data1;
+	object->list_data.int_data = data1;
+	object->month = month;
 }
 
 int main(int argc, char* argv[]) {
 	cout << "program begein" << endl;
 
 	List firstObject;
-	list_generator(&firstObject, NULL, NULL, 100);
+	list_generator(&firstObject, NULL, NULL, 100, jan);
 
 	List* secondObject = new List;
-	list_generator(secondObject, NULL, NULL, 50);
+	list_generator(secondObject, NULL, NULL, 50, feb);
 
 	List* thirdObject = (List*) malloc(sizeof(List));
-	list_generator(thirdObject, NULL, NULL, 25);
+	list_generator(thirdObject, NULL, NULL, 25, mar);
 
 	/* produce 50 lists with a value */
 
-
 	/* checking area */
-	cout << firstObject.data1 << endl;
-	cout << secondObject->data1 << endl;
-	cout << thirdObject->data1 << endl;
+	cout << firstObject.list_data.int_data << endl;
+	cout << secondObject->list_data.int_data << endl;
+	cout << thirdObject->list_data.int_data << endl;
+
 
 	cout << "program terminated" << endl;
 
