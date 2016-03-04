7a441cbc0bcb128d036d8485d1957cd6610755f9
Isdemus <isdemus@gmail.com>
2016-03-03 23:56:57 -0500

    constructor/destructor

--- abc#1.cpp
+++ abc#1.cpp
@@ -11,7 +11,6 @@ enum month_t {jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec};
 
 union data {
 	int int_data;
-	//string string_data;
 	char char_data;
 	bool bool_data;
 };
@@ -21,12 +20,20 @@ typedef struct list{
 	struct list* prev;
 	union data list_data;
 	enum month_t month;
+
+	list() { //default constructor
+		this->next = NULL;
+		this->prev = NULL;
+	}
+
+	//assumption: list is a pointer with malloc use
+	~list() {
+		free(this);
+	}
 } List;
 
 //automated function to build a list
-void list_generator(List* object, List* next, List* prev, int data1, enum month_t month) {
-	object->next = next;
-	object->prev = prev;
+void list_generator(List* object, int data1, enum month_t month) {
 	object->list_data.int_data = data1;
 	object->month = month;
 }
@@ -34,19 +41,19 @@ void list_generator(List* object, List* next, List* prev, int data1, enum month_
 int main(int argc, char* argv[]) {
 	cout << "program begein" << endl;
 
-	List firstObject;
-	list_generator(&firstObject, NULL, NULL, 100, jan);
+	//List firstObject;
+	//list_generator(&firstObject, 100, jan);
 
 	List* secondObject = new List;
-	list_generator(secondObject, NULL, NULL, 50, feb);
+	list_generator(secondObject, 50, feb);
 
 	List* thirdObject = (List*) malloc(sizeof(List));
-	list_generator(thirdObject, NULL, NULL, 25, mar);
+	list_generator(thirdObject, 25, mar);
 
 	/* produce 50 lists with a value */
 
 	/* checking area */
-	cout << firstObject.list_data.int_data << endl;
+	//cout << firstObject.list_data.int_data << endl;
 	cout << secondObject->list_data.int_data << endl;
 	cout << thirdObject->list_data.int_data << endl;
 
