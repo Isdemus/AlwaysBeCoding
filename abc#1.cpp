b466ade7741682ce005f544e5118418738572a76
Isdemus <isdemus@gmail.com>
2016-03-03 23:30:09 -0500

    typedef struct

--- abc#1.cpp
+++ abc#1.cpp
@@ -6,14 +6,14 @@
 #include <iostream>
 using namespace std;
 
-struct list {
-	list* next;
-	list* prev;
-	int data1; 
-};
+typedef struct list{
+	struct list* next;
+	struct list* prev;
+	int data1;
+} List;
 
 //automated function to build a list
-void list_generator(struct list* object, struct list* next, struct list* prev, int data1) {
+void list_generator(List* object, List* next, List* prev, int data1) {
 	object->next = next;
 	object->prev = prev;
 	object->data1 = data1;
@@ -22,14 +22,17 @@ void list_generator(struct list* object, struct list* next, struct list* prev, i
 int main(int argc, char* argv[]) {
 	cout << "program begein" << endl;
 
-	struct list firstObject;
+	List firstObject;
 	list_generator(&firstObject, NULL, NULL, 100);
 
-	struct list* secondObject = new list;
+	List* secondObject = new List;
 	list_generator(secondObject, NULL, NULL, 50);
 
-	struct list* thirdObject = (list*) malloc(sizeof(struct list));
+	List* thirdObject = (List*) malloc(sizeof(List));
 	list_generator(thirdObject, NULL, NULL, 25);
+
+	/* produce 50 lists with a value */
+
 
 	/* checking area */
 	cout << firstObject.data1 << endl;
