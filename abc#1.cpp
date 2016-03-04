b6c5ff1a93846a044d9a60b93f3014b4e9df01f7
Isdemus <isdemus@gmail.com>
2016-03-04 00:18:20 -0500

    linked_list completed

--- abc#1.cpp
+++ abc#1.cpp
@@ -18,6 +18,8 @@ union data {
 typedef struct list{
 	struct list* next;
 	struct list* prev;
+	struct list* first;
+	struct list* last;
 	union data list_data;
 	enum month_t month;
 
@@ -33,8 +35,8 @@ typedef struct list{
 } List;
 
 //automated function to build a list
-void list_generator(List* object, int data1, enum month_t month) {
-	object->list_data.int_data = data1;
+void list_generator(List* object, int data, enum month_t month) {
+	object->list_data.int_data = data;
 	object->month = month;
 }
 
@@ -51,12 +53,42 @@ int main(int argc, char* argv[]) {
 	list_generator(thirdObject, 25, mar);
 
 	/* produce 50 lists with a value */
+	int initial_month = 0;
+	int initial_data  = 1;
+	struct list* first = NULL;
+	struct list* last  = NULL;
+	struct list* previous = NULL;
+	for(int i=0; i<50; i++) {
+		List* randomObject = new List;
+		list_generator(randomObject, initial_data, (enum month_t) initial_month);
+		if(i==0) { first = randomObject; }
+		else if(i==49) { last = randomObject; }
+
+		randomObject->first = first;
+		randomObject->last  = last;
+
+		if(previous != NULL) {
+			randomObject->prev = previous;
+			previous->next = randomObject;
+		}
+
+		previous = randomObject;
+		if(initial_month==11) { initial_month = 0; }
+		else { initial_month += 1; }
+		initial_data += 1;
+	}
+
 
 	/* checking area */
-	//cout << firstObject.list_data.int_data << endl;
 	cout << secondObject->list_data.int_data << endl;
 	cout << thirdObject->list_data.int_data << endl;
 
+	/* linked list checking area */
+	while(first != last) {
+		cout << first->list_data.int_data << endl;
+		first = first->next;
+	}
+	if(first == last) {	cout << first->list_data.int_data << endl; }
 
 	cout << "program terminated" << endl;
 
