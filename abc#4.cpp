813418e6e87ddea68d8778a1092ee630c2a6ec01
Isdemus <isdemus@gmail.com>
2016-03-07 00:16:44 -0500

    custom_queue initial

---
+++ abc#4.cpp
@@ -0,0 +1,20 @@
+// abc#4
+// implementing stack, queue, vector using an array
+
+#include <iostream>
+using namespace std;
+
+class custom_queue {
+
+};
+
+int main(int argc, char* argv[]) {
+
+	cout << "Program Begin\n" << endl;
+
+
+
+	cout << "\nProgram Terminate" << endl;
+
+	return 0;
+}
