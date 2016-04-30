445dd2cb5ba7a6d30e75ff6935f8ff7a09c11971
Isdemus <isdemus@gmail.com>
2016-04-30 16:54:19 +0900

    organize

---
+++ GCJ/SavingTheUniverse/Nicholas/gcj01.cc
@@ -0,0 +1,12 @@
+/* 
+Google Code Jam Entrance Test 
+(Saving the universe: https://code.google.com/codejam/contest/32013/dashboard)
+
+Used Algorithm Method: Dynamic Programming
+Coded by isdemus (April 29 2016)
+*/
+
+#include <iostream>
+#include <map>
+using namespace std;
+
