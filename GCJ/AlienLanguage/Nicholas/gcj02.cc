daf6750a64c3da7ad30df13a64c37e73c960af0e
Isdemus <isdemus@gmail.com>
2016-05-02 15:16:07 +0900

    initial

---
+++ GCJ/AlienLanguage/Nicholas/gcj02.cc
@@ -0,0 +1,15 @@
+
+/* 
+Google Code Jam Entrance Test 
+(Saving the universe: https://code.google.com/codejam/contest/90101/dashboard)
+
+Used Algorithm Method: 
+Coded by isdemus (May 03 2016)
+*/
+
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+	
+	return 0;
+}
