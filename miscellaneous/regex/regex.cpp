8e7bfeb4a67cb9ee66e7aa803fd0bf6f04483afc
Isdemus <isdemus@gmail.com>
2016-08-03 22:36:01 +0900

    basic regex practice

---
+++ miscellaneous/regex/regex.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <regex>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+  std::regex music_regex3("play\\s+((some|any)\\s+)*(.+)(music|song)[a-z]*"); 
+  std::regex music_regex4(".*(let me listen to|play)\\s+(.+)\\s+by\\s+(.+)", std::regex::ECMAScript|std::regex::icase);
+
+  std::vector<std::regex> reg;
+  reg.push_back(music_regex3);
+  reg.push_back(music_regex4);
+  
+  std::smatch base_match;
+
+  std::string test = "play Adele's Chasing";
+//  test = "play music";
+  test = "play some hip hop music";
+
+  while (getline(cin, test)) {
+    for (std::regex regexp : reg) {
+      if (std::regex_match(test, base_match, regexp)) {;
+        for (size_t i = 0; i < base_match.size(); ++i) {
+          std::ssub_match sub_match = base_match[i];
+          std::string piece = sub_match.str();
+          std::cout << "  submatch " << i << ": " << piece << '\n';
+        }     
+      } else {
+        cout << "cannot catch the sentence" << endl;
+      }
+    }
+  }
+
+  return 0;
+}
