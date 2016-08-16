cf5bb4f5725269b94605f57adc97724a4ddd82e2
Isdemus <isdemus@gmail.com>
2016-08-16 22:31:24 +0900

    regex for music

--- miscellaneous/regex/regex.cpp
+++ miscellaneous/regex/regex.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 #include <regex>
 #include <vector>
+#include <memory>
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
-  std::regex music_regex3("play\\s+((some|any)\\s+)*(.+)(music|song)[a-z]*"); 
-  std::regex music_regex4(".*(let me listen to|play)\\s+(.+)\\s+by\\s+(.+)", std::regex::ECMAScript|std::regex::icase);
+  auto ptr1 = unique_ptr<vector<string>>(new vector<string>());
+  auto ptr2 = unique_ptr<vector<string>>(new vector<string>());
+//  std::regex music_regex3("play\\s+((some|any)\\s+)*(.+)(music|song)[a-z]*"); 
+//  std::regex music_regex4(".*(let me listen to|play)\\s+(.+)\\s((music|song)\\s)*(played|sang|sung\\s)*by\\s+(.+)", std::regex::ECMAScript|std::regex::icase);
+
+  std::regex music_regex3("play[:space:]+((some|any)[:space:]+)*(.+)(music|song)[a-z]*"); 
+  std::regex music_regex4(".*(let me listen to|play)[:space:]+(.+)[:space:]((music|song)[:space:])*(played|sang|sung[:space:])*by[:space:]+(.+)", std::regex::ECMAScript|std::regex::icase);
+
+
+  std::regex musicRegex1("play\\s+((some|any)\\s+)*(.+)(music|song)[a-z]*", std::regex::ECMAScript|std::regex::icase); 
+
 
   std::vector<std::regex> reg;
   reg.push_back(music_regex3);
