69b4c58c5d0f0efe615a82518b38842ee5976f49
isdemus <isdemus@gmail.com>
2017-02-28 23:47:10 -0500

    Fixed the code to work without an input file + used a set to store buy/sell dates

--- randomProblems/stockProfit.cc
+++ randomProblems/stockProfit.cc
@@ -11,20 +11,22 @@
 #include <iostream>
 #include <fstream>
 #include <array>
+#include <set>
+
+#define INPUTFILE 0
+#define ARRSIZE(arr) (sizeof(arr) / sizeof(*arr))
 
 using namespace std;
 
-/* 
- *	Printing all values in the array with indexes as multiples of 30
- */
-template<std::size_t SIZE>
-void printvalues(array<int, SIZE> stocklist, int infocount) {
-	for (int index = 0; index < infocount; index++) {
-		cout << "index : " << index << " value : " << stocklist[30 * index] << endl;
+// Printing all values in the array with indexes as multiples of 30
+void printvalues(const int stocklist[], const int leng) {
+	for (int index = 0; index < leng; index++) {
+		cout << "index : " << index << " value : " << stocklist[index] << endl;
 	}
 }
 
 int main(int argc, char* argv[]) {
+#if INPUTFILE
 	int input;
 	string filename = "stock.txt";
 	ifstream filetoread (filename, ios::in);
@@ -38,36 +40,50 @@ int main(int argc, char* argv[]) {
 	int infocount = 0;
 	int startmin  = 0;
 
-	// One example would be that [7, 5, 25, 4, 34, 20, 23, 45, 29, 1, 45, 23]
+	// One example would be that [7, 51, 25, 4, 34, 20, 23, 45, 29, 1, 45, 23]
 	// With each index as a multiple of 30 min
 	while (filetoread >> input) {
 		stocklist[startmin] = input;
 		startmin += 30;
 		infocount += 1;
 	}
+#endif
+
+	int stocklist[] = {7, 51, 25, 4, 34, 20, 23, 45, 29, 1, 45, 23};
 
 	// Checking up the list of stock values
-	printvalues(stocklist, infocount);
+	printvalues(stocklist, ARRSIZE(stocklist));
 
-/*
- *	Testing : with above input, it will produce 44 (buying at $1 and selling at $45)
- */
+	// Testing : with above input, it will produce 44 (buying at $1 and selling at $45)
 	// If there exists less than 2 data in the array, output an error msg
-	if (infocount < 2) {
+	if (ARRSIZE(stocklist) < 2) {
 		cerr << "Not enough data given" << endl;
 		return -1;
 	}
 	
-	int min = stocklist[0];
-	int profit = stocklist[30] - min;
-	cout << "begin profit : " << profit << endl;
-	for (int index = 1; index < infocount; index++) {
-		int stockvalue = stocklist[index * 30];
-		profit = profit > stockvalue - min ? profit : stockvalue - min;
-		min = min < stockvalue ? min : stockvalue;
-	}
+	int minindex = 0;
+	int profit = stocklist[minindex + 1] - stocklist[minindex];
+	set<pair<int, int>> best_interval;
+	best_interval.insert(make_pair(minindex, minindex + 1));
 
-	cout << "Max profit is : " << profit << endl;
+	cout << "begin profit : " << profit << endl;
+	for (int index = 1; index < ARRSIZE(stocklist); index++) {
+		int stockvalue = stocklist[index];
+		if (profit <= stockvalue - stocklist[minindex]) {
+			profit = stockvalue - stocklist[minindex];
+			best_interval.insert(make_pair(minindex, index));
+		}
+		minindex = (stocklist[minindex] < stockvalue ? minindex : index);
+	}
+	cout << "Max profit is : " << profit << endl << endl;
+
+	for_each(best_interval.begin(), best_interval.end(), [=] (pair<int, int> per) {
+		if (stocklist[per.second] - stocklist[per.first] == profit) {
+			cout << "Buying at index : " << per.first << " at a price : " << stocklist[per.first] << endl;
+			cout << "Selling at index : " << per.second << " at a price : " << stocklist[per.second] << endl;
+			cout << endl;
+		}
+	});
 	
 	return 0;
 }
